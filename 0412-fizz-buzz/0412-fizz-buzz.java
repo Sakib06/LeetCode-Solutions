b880392c93de395099633f44bf864276c33a7444
Nazmus Sakib Fahim <68812993+Sakib06@users.noreply.github.com>
2026-07-29 20:33:07 +0600

    Time: 1 ms (99.9%), Space: 46.9 MB (46.07%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,21 +1,13 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<>();
-        int[] arr = new int[n];
 
-        for(int i=0; i<n; i++) {
-            arr[i] = i+1;
-        }
-
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 1; i <= n; i++) {
             
-            if(arr[i] % 3 == 0 && arr[i] % 5 == 0) result.add("FizzBuzz");
-
-            else if(arr[i] % 3 == 0) result.add("Fizz");
-
-            else if(arr[i] % 5 == 0) result.add("Buzz");
-
-            else result.add(i+1 + "");
+            if(i % 3 == 0 && i % 5 == 0) result.add("FizzBuzz");
+            else if(i % 3 == 0) result.add("Fizz");
+            else if(i % 5 == 0) result.add("Buzz");
+            else result.add(String.valueOf(i));
         }
 
         return result;
